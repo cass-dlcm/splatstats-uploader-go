b6e4f4518a02b6dc9cab9aff6981f96dd192c5bd
Cassandra de la Cruz-Munoz <cassandra.delacruzmunoz@gmail.com>
2021-05-18 10:29:07 -0400

    add getting api token

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module cass-dlcm.dev/splatstatsuploader
 
 go 1.16
 
-require github.com/spf13/viper v1.7.1
+require (
+	github.com/hashicorp/go-version v1.3.0
+	github.com/spf13/viper v1.7.1
+	golang.org/x/sys v0.0.0-20210514084401-e8d321eab015 // indirect
+	golang.org/x/term v0.0.0-20210503060354-a79de5458b56 // indirect
+)
--- splatstatsuploader.go
+++ splatstatsuploader.go
@@ -1,33 +1,50 @@
-package splatstatsuploader
+package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"regexp"
 
+	"github.com/hashicorp/go-version"
 	"github.com/spf13/viper"
+	"golang.org/x/term"
 )
 
 var A_VERSION = "1.5.10"
-var B_VERSION = "1.0.0"
+var B_VERSION = "1.0.1"
 
 func CheckForUpdates() {
-	latest_script, err := http.get(
-		"https://raw.githubusercontent.com/cass-dlcm/splatstats-uploader-go/main/splatstatsuploader.go"
-	)
-	if err {
+	latest_script, err := http.Get("https://raw.githubusercontent.com/cass-dlcm/splatstats-uploader-go/main/splatstatsuploader.go")
+	if err != nil {
 		fmt.Println("Error retrieving the latest version.")
 	}
 	defer latest_script.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	re := regexp.MustCompile("B_VERSION = \"([\d.]*)\"")
-	new_version := re.FindString(body)
-	fmt.Println(new_version)
+	body, _ := io.ReadAll(latest_script.Body)
+	re := regexp.MustCompile("B_VERSION = \"([\\d.]*)\"")
+	new_version := re.FindString(string(body))
+	v1, err := version.NewVersion(B_VERSION)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+	v2, err := version.NewVersion(new_version[13 : len(new_version)-1])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+	if v1.LessThan(v2) {
+		fmt.Println("New version availbile at https://github.com/cass-dlcm/splatstats-uploader-go.")
+		fmt.Println("Please download the new version before continuing.")
+		latest_script.Body.Close()
+		os.Exit(0)
+	}
 }
 
 func SetLanguage() {
 	fmt.Println("Default locale is en-US. Press Enter to accept, or enter your own (see readme for list).")
 	var locale string
-
 	// Taking input from user
 	fmt.Scanln(&locale)
 	if locale == "" {
@@ -52,8 +69,29 @@ func SetLanguage() {
 			fmt.Scanln(&locale)
 			_, exists = language_list[locale]
 		}
+		viper.Set("user_lang", locale)
 	}
-	viper.Set("user_lang", locale)
+	viper.WriteConfig()
+}
+
+func SetApiToken() {
+	var username string
+	fmt.Println("SplatStats username: ")
+	fmt.Scanln(&username)
+	password, _ := term.ReadPassword(int(os.Stdin.Fd()))
+	url := "http://localhost:8000/auth/api-token/"
+	client := &http.Client{}
+	auth_json, _ := json.Marshal(map[string]string{
+		"username": username, "password": string(password),
+	})
+	auth_body := bytes.NewReader(auth_json)
+	req, _ := http.NewRequest("POST", url, auth_body)
+	req.Header.Set("Content-Type", "application/json")
+	resp, _ := client.Do(req)
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(resp.Body)
+	newStr := buf.String()
+	viper.Set("api_key", newStr[10:len(newStr)-2])
 	viper.WriteConfig()
 }
 
@@ -70,8 +108,17 @@ func main() {
 	viper.SetConfigType("yaml")
 
 	if err := viper.ReadInConfig(); err != nil {
-		fmt.Printf("Error reading config file, %s", err)
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			// Config file not found; ignore error if desired
+			fmt.Println("No config file found. One will be created.")
+		} else {
+			// Config file was found but another error was produced
+			fmt.Println("Error reading the config file.")
+			os.Exit(1)
+		}
 	}
+
+	// Config file found and successfully parsed
 
 	// Set undefined variables
 	viper.SetDefault("api_key", "")
@@ -79,7 +126,11 @@ func main() {
 	viper.SetDefault("session_token", "")
 	viper.SetDefault("user_lang", "")
 
-	if !(viper.IsSet("user_lang")) {
+	if !(viper.IsSet("api_key")) {
+		SetApiToken()
+	}
+
+	if !(viper.IsSet("user_lang")) || viper.GetString("user_lang") == "" {
 		SetLanguage()
 	}
 }
