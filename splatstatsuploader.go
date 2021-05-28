dfd548e993917c28a019be4798981a859dd7286f
Cassandra de la Cruz-Munoz <cassandra.delacruzmunoz@gmail.com>
2021-05-28 18:16:38 -0400

    bump version number to force update

--- splatstatsuploader.go
+++ splatstatsuploader.go
@@ -18,7 +18,7 @@ import (
 	"golang.org/x/term"
 )
 
-var progVersion = "1.4.2"
+var progVersion = "1.4.3"
 
 func checkForUpdates() {
 	latestScript, err := http.Get("https://raw.githubusercontent.com/cass-dlcm/splatstats-uploader-go/main/splatstatsuploader.go")
